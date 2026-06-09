7b3d6a6e1b819e3e4c97f124d8b3446d0826d858
Sneha Singh <snehasingh240903@gmail.com>
2026-06-09 22:26:16 +0530

    completed CRUD and dashboard setup

--- frontend/EduNexus/src/App.jsx
+++ frontend/EduNexus/src/App.jsx
@@ -6,18 +6,24 @@ function App() {
   const [editingStudent, setEditingStudent] = useState(null);
 
   return (
-    <>
-      <AddStudent
-        editingStudent={editingStudent}
-        setEditingStudent={setEditingStudent}
-      />
+    <div className="min-h-screen bg-slate-100 p-6">
+      <h1 className="text-4xl font-bold text-blue-600 mb-6">
+        EduNexus Dashboard
+      </h1>
 
-      <hr />
+      <div className="bg-white p-6 rounded-xl shadow-md">
+        <AddStudent
+          editingStudent={editingStudent}
+          setEditingStudent={setEditingStudent}
+        />
+      </div>
 
-      <StudentList
-        setEditingStudent={setEditingStudent}
-      />
-    </>
+      <div className="bg-white p-6 rounded-xl shadow-md mt-6">
+        <StudentList
+          setEditingStudent={setEditingStudent}
+        />
+      </div>
+    </div>
   );
 }
 
