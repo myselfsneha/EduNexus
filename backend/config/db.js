c8b15fd4dce56e92dc994c1661c0086e015a12db
Sneha Singh <snehasingh240903@gmail.com>
2026-06-10 14:11:11 +0530

    connect aiven mysql

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,13 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 connection.connect((err) => {
@@ -12,6 +16,7 @@ connection.connect((err) => {
     console.error("Database connection failed:", err);
     return;
   }
+
   console.log("MySQL Connected");
 });
 
